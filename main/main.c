05056caa2b50226a2f80184127cff3dde918b204
DESKTOP-6R2O3C2\CXC <robo.12@outlook.es>
2021-12-23 12:19:19 -0600

    In this example learn how to use structures and practice

--- main/main.c
+++ main/main.c
@@ -1,46 +1,41 @@
 #include <stdio.h>
-#include "esp_log.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_system.h"
+#include <string.h>
 
-#define TAG "DICE"
+typedef struct  {
+  char firstName[20];
+  char lastName[20];
+  int age;
+}Person;
 
-void example_logs()
-{
- esp_log_level_set("LOG",ESP_LOG_INFO);
+struct Person {
+  char firstName[20];
+  char lastName[20];
+  int age;
+};
 
- ESP_LOGE("LOG","This is an error");
- ESP_LOGW("LOG","This is a warning");
- ESP_LOGI("LOG","This is an info");
- ESP_LOGD("LOG","This is a debbug");
- ESP_LOGV("LOG","This is verbose");
-
- int number = 0 ;
- ESP_LOGE("TAG 2 ","This is an error %d ",number++);
- ESP_LOGW("TAG 2","This is a warning %d",number++);
- ESP_LOGI("TAG 2","This is an info %d",number++);
- ESP_LOGD("TAG 2","This is a debbug %d ",number++);
- ESP_LOGV("TAG 2","This is verbose %d ",number++);
-
+//This is an example to understand how the memory is updated.
+void updatePerson(Person *person){ // with this we can access to the same location of memory and dont make a copy 
+  strcpy(person->firstName,"Bob");
+  strcpy(person->lastName,"fisher");
+  person->age = 35;
 }
-
-int dice_role()
-{
-  int random = esp_random();
-  int positiveNumber = abs(random);
-  int diceNumber = (positiveNumber%6)+1;
-  return diceNumber;
-}
-
+//
+//void exclamIt(char *phrase){ // if you dont use * or [] it doent work
+//  sprintf(phrase, "%s!",phrase);
+//} // is the same use phrase[]
 
 void app_main(void)
 {
-   int i = 0 ;
-   while(1)
-   {
-     vTaskDelay(1000/portTICK_PERIOD_MS);
-     ESP_LOGI(TAG,"random number %d ",esp_random());
-   }
+  Person person;
+  //char phrase[20] = {"Hello world"};
+  //exclamIt(phrase);
 
+  struct Person personx;
+  strcpy(personx.firstName,"Erick");
+  strcpy(person.lastName,"fisher");
+  personx.age = 35;
+ 
+  updatePerson(&person);
+  printf("person : %s %s is %d yeards old \n",personx.firstName, personx.lastName,personx.age);  
 }
+ 
