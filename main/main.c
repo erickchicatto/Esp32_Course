ac0be370ccba17b17aa38e0526ea36c35db9b6ae
DESKTOP-6R2O3C2\CXC <robo.12@outlook.es>
2021-12-23 13:40:49 -0600

    Add some features about using functions pointers

--- main/main.c
+++ main/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h> // in order to allocate memory 
 
 typedef struct  {
   char firstName[20];
@@ -7,35 +8,34 @@ typedef struct  {
   int age;
 }Person;
 
-struct Person {
-  char firstName[20];
-  char lastName[20];
-  int age;
-};
-
 //This is an example to understand how the memory is updated.
-void updatePerson(Person *person){ // with this we can access to the same location of memory and dont make a copy 
-  strcpy(person->firstName,"Bob");
-  strcpy(person->lastName,"fisher");
+void updatePerson(Person *person, char * dataRetreieved){ // with this we can access to the same location of memory and dont make a copy 
+  strcpy(person->firstName,"data from dataRetreieved");
+  strcpy(person->lastName,"data from dataRetreieved");
   person->age = 35;
 }
-//
-//void exclamIt(char *phrase){ // if you dont use * or [] it doent work
-//  sprintf(phrase, "%s!",phrase);
-//} // is the same use phrase[]
+void connectAndGetInfo(char *url,void(*DoWork)(char *dataRetreieved)){
+  //connect to wireless
+  //connect to end point
+  //allocate memory 
+  char *dataRetreieved=(char *)malloc(1024);
+  //fill buffer with data : getRequest(&dataRetreived);
+  //do work and geta person object 
+  DoWork(dataRetreieved);
+  //clean up memory and other resources
+  free((void*)dataRetreieved);
+}
+
+void DoWork(char *dataRetreieved){
+  Person person;
+  updatePerson(&person,dataRetreieved);
+  printf("person : %s %s is %d yeards old \n",personx.firstName, personx.lastName,personx.age);  
+}
 
 void app_main(void)
 {
-  Person person;
-  //char phrase[20] = {"Hello world"};
-  //exclamIt(phrase);
 
-  struct Person personx;
-  strcpy(personx.firstName,"Erick");
-  strcpy(person.lastName,"fisher");
-  personx.age = 35;
- 
-  updatePerson(&person);
-  printf("person : %s %s is %d yeards old \n",personx.firstName, personx.lastName,personx.age);  
+
+  connectAndGetInfo("https:getperson.com",DoWor);
 }
  
