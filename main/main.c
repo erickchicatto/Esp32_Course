0ce92169907ae9c3ae1d6a6a743d6dc27486398f
DESKTOP-6R2O3C2\CXC <robo.12@outlook.es>
2021-12-24 11:04:53 -0600

    Add functions task in order one task can send and the other can received.

--- main/main.c
+++ main/main.c
@@ -5,29 +5,30 @@
 #include "freertos/task.h"
 #include "esp_system.h"
 
-void task1(){
-  while(true){
-    printf("reading temperature \n ");
-    vTaskDelay(1000/portTICK_PERIOD_MS);
+static TaskHandle_t receiverHandler = NULL;
+
+void sender(void* params){
+  while(true)
+  {
+   xTaskNotifyGive(receiverHandler);
+   xTaskNotifyGive(receiverHandler);
+   xTaskNotifyGive(receiverHandler);
+   xTaskNotifyGive(receiverHandler);
+   vTaskDelay(5000/portTICK_PERIOD_MS);
   }
 }
 
-void task2(void *params){
-  while(true){
-    printf("reading humedicity %s \n",(char *)params);
-    vTaskDelay(1000/portTICK_PERIOD_MS);
+void receiver(void* params){
+  while(true)
+  {
+    int count = ulTaskNotifyTake(pdFALSE,portMAX_DELAY);
+    printf("received notification %d times \n",count);
   }
 }
-
-
 
 
 void app_main(void)
 {
-   //task1(); This will not working 
-   //task2(); This will not working 
-   xTaskCreate(&task1,"temperature reading ",2048,NULL,2,NULL);
-   xTaskCreatePinnedToCore((&task2),"humidity reading",2048,"task 2 ",2,NULL,1);
-
-
+   xTaskCreate(&receiver,"sender",2048,NULL,2,&receiverHandler);
+   xTaskCreate(&sender,"receiver",2048,NULL,2,NULL);
 }
