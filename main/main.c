16e21058d44fa81279d3e77b90d12a377e822435
DESKTOP-6R2O3C2\CXC <robo.12@outlook.es>
2021-12-26 13:08:34 -0600

    binary semaphore

--- main/main.c
+++ main/main.c
@@ -4,44 +4,23 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
-
 #include "freertos/semphr.h"
 
-xSemaphoreHandle mutexBus;
+/**Binary semaphore is used to indicate that something is happen.*/
+xSemaphoreHandle binSemaphore;
 
-void writeToBus(char * message){
-    printf(message);
-}
-
-void task1(){
+void listenForHTTP(void *params){
   while(true){
-    printf("reading temperature \n ");
-    if(xSemaphoreTake(mutexBus,1000/portTICK_PERIOD_MS))
-    {
-      writeToBus("temperature is 25c \n");
-      xSemaphoreGive(mutexBus);// to make mutex available
-    }
-    else
-    {
-      printf("reading temperature timed out \n");
-    }
-    vTaskDelay(1000/portTICK_PERIOD_MS);
+    printf("received HTTP message \n ");
+    xSemaphoreGive(binSemaphore);
+    vTaskDelay(5000/portTICK_PERIOD_MS);
   }
 }
 
 void task2(){
   while(true){
-    printf("reading humedicity \n ");
-    if(xSemaphoreTake(mutexBus,1000/portTICK_PERIOD_MS))
-    {
-      writeToBus("humedicity is 50c \n");
-      xSemaphoreGive(mutexBus);// to make mutex available
-    }
-    else
-    {
-      printf("writting humedicity timed out \n");
-    }
-    vTaskDelay(1000/portTICK_PERIOD_MS);
+    xSemaphoreTake(binSemaphore,portMAX_DELAY);
+    printf("doing something with http \n ");
   }
 }
 
@@ -49,12 +28,10 @@ void task2(){
 void app_main(void)
 {
    //task1(); This will not working 
-   //task2(); This will not working
-   mutexBus = xSemaphoreCreateMutex();
-
-
-   xTaskCreate(&task1,"temperature reading ",2048,NULL,2,NULL);
-   xTaskCreate(&task2,"humidity reading",2048,NULL,2,NULL);
+   //task2(); This will not working 
+   binSemaphore = xSemaphoreCreateBinary();
    
-
+   xTaskCreate(&listenForHTTP,"temperature reading ",2048,NULL,2,NULL);
+   xTaskCreate(&task2,"do something with the http",2048,NULL,2,NULL);
+      
 }
