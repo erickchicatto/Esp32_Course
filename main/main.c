40b56823d20bc06e2aad6e724eb67f71397a8e8c
DESKTOP-6R2O3C2\CXC <robo.12@outlook.es>
2022-01-22 13:06:58 -0600

    Study concepts from freeRtos

--- main/main.c
+++ main/main.c
@@ -3,6 +3,8 @@
 #include "freertos/task.h"
 #include "freertos/semphr.h"
 #include "freertos/timers.h"
+#include "freertos/queue.h"
+#include "freertos/event_groups.h"
 #include "esp_system.h"
 
 //Handle for a Task
@@ -14,8 +16,16 @@ xSemaphoreHandle mutexBus;
 xSemaphoreHandle binSemaphore;
 
 //queue
+struct AMessage
+{
+  char ucMessageID;
+  char ucData[20];
+}
+
 //event group
-//Example enddianes using pointers in C 
+
+
+//Example enddianes using pointers in C and apply to this board 
 
 
 // To study eventGroups : https://www.freertos.org/RTOS_Task_Notification_As_Event_Group.html
@@ -28,6 +38,7 @@ void on_timer(TimerHandle_t xTimer)
 {
     printf("time hit %lld \n",esp_timer_get_time() /1000);
 }
+
 /*For a simple task */
 void task1()
 {
@@ -80,19 +91,53 @@ void task4()
 }
 
 
+void vTask(void *pvParameters)
+{
+  QueueHandle_t xQueue1, xQueue2;
+  
+  //Create a queue capable of containing  10 unsigned long values
+  xQueue1 = xQueueCreate(10,sizeof(unsigned long));
+
+  if(xQueue1 == NULL)
+  {
+   /*Queue is not created*/
+  }
+  
+  xQueue2 = xQueueCreate( 10, sizeof( struct AMessage * ) );
+
+  if(xQueue2 == NULL)
+  {
+   /*Queue is not created*/
+  }
+
+  printf("All works ok \n");
+
+}
 
 
 void app_main(void)
 {
   //Simple task
   xTaskCreate(&task1,"example for task ",2048,NULL,2,NULL);
+  
+  
   //Task using semaphore
   mutexBus = xSemaphoreCreateMutex();
   xTaskCreate(&task2,"example for semaphore ",2048,NULL,2,NULL);
+  
+  
+  
   //Create a binSemaphore
   binSemaphore = xSemaphoreCreateBinary();
   xTaskCreate(&task3,"example for semaphore binary",2048,NULL,2,NULL);
   xTaskCreate(&task4,"example for semaphore binary",2048,NULL,2,NULL);
+  
+  //Queue 
+  xTaskCreate(&vTask,"example for a queue",2048,NULL,2,NULL);
+
+  //Event Group
+  //http://www.openrtos.net/xEventGroupWaitBits.html    
+
 
   //This is for the timer 
   printf("app started %lld \n",esp_timer_get_time() /1000);
