7412d17cce0afdec4615258a7fe9c35a76fe09db
DESKTOP-6R2O3C2\CXC <robo.12@outlook.es>
2022-01-31 18:45:51 -0600

    Add functionality of high timer resolution

--- main/main.c
+++ main/main.c
@@ -1,18 +1,41 @@
 #include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/timers.h"
-#include "esp_system.h"
+#include "esp_timer.h"
+#include "driver/gpio.h"
+#include "freertos/FreeRtos.h"
+#include "freertos/task.h"
 
-// To study eventGroups : https://www.freertos.org/RTOS_Task_Notification_As_Event_Group.html
-
-void on_timer(TimerHandle_t xTimer)
-{
-    printf("time hit %lld \n",esp_timer_get_time() /1000);
+void timer_callback(void *args){
+  //printf("fired after 20 ms \n");
+  static bool on ;
+  on = !on;
+  gpio_set_level(GPIO_NUM_4,on);
 }
 
 void app_main(void)
 {
-  printf("app started %lld \n",esp_timer_get_time() /1000);
-  TimerHandle_t xTimer = xTimerCreate("my timer",pdMS_TO_TICKS(1000),true,NULL,on_timer);
-  xTimerStart(xTimer,0);
+    gpio_pad_select_gpio(GPIO_NUM_4);
+    gpio_set_direction(GPIO_NUM_4,GPIO_MODE_OUTPUT);
+    
+    const esp_timer_create_args_t esp_timer_create_args = {
+      .callback = timer_callback;
+      .name = "my timer";
+    };
+
+    esp_timer_handle_t esp_timer_handler;
+    esp_timer_create(&esp_timer_create_args,&esp_timer_handler);
+    //esp_timer_start_once(esp_timer_handler,20);
+    esp_timer_start_periodic(esp_timer_handle,50);
+
+   int x = 0;
+   
+   while(true){
+      esp_timer_dump(stdout);
+      vTaskDelay(pdMS_TO_TICKS(1000));
+      if(x==5){
+        esp_timer_stop(esp_timer_handle);
+        esp_timer_delete(esp_timer_handle);
+      }
+   }
+
+
 }
