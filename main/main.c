67e6deacfb845b434c7f6353baf3c9ec4feaaf43
DESKTOP-6R2O3C2\CXC <robo.12@outlook.es>
2022-02-12 14:43:01 -0600

    When you have problems with the debugger follow this steps : a) Install again the drivers in teh zadig software b) Run idf.py build c) Run idf.py flash
    and then run the command to start the debbuger then you can start the debugger session.

--- main/main.c
+++ main/main.c
@@ -1,141 +1,96 @@
 #include <stdio.h>
-#include<string.h>    //strlen
+#include <string.h>  
+#include <stdlib.h> 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/gpio.h"
 
-#include "esp_wifi.h"
-#include "esp_event_loop.h"
-#include "freertos/event_groups.h"
-#include "esp_system.h"
-#include "esp_log.h"
 
-#include "lwip/err.h"
-#include "lwip/sockets.h"
-#include "lwip/sys.h"
-#include "lwip/netdb.h"
-#include "lwip/dns.h"
+#define GPIO_OUTPUT_IO_0    18
+#define GPIO_OUTPUT_IO_1    19
+#define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_OUTPUT_IO_0) | (1ULL<<GPIO_OUTPUT_IO_1))
+#define GPIO_INPUT_IO_0     4
+#define GPIO_INPUT_IO_1     5
+#define GPIO_INPUT_PIN_SEL  ((1ULL<<GPIO_INPUT_IO_0) | (1ULL<<GPIO_INPUT_IO_1))
+#define ESP_INTR_FLAG_DEFAULT 0
 
-#define SSID "SSID"
-#define PASSPHARSE "PASSWORD"
-#define MESSAGE "HelloTCPServer"
-#define TCPServerIP "IP of the TCP server"
+static xQueueHandle gpio_evt_queue = NULL;
 
-static EventGroupHandle_t wifi_event_group;
-const int CONNECTED_BIT = BIT0;
-static const char *TAG = "tcp_client";
-
-void wifi_connect()
+static void IRAM_ATTR gpio_isr_handler(void *arg)
 {
-  wifi_config_t cfg = {
-    .sta={
-      .ssid = SSID,
-      .password = PASSPHARSE,
-    },
-  };
-  
-  ESP_ERROR_CHECK(esp_wifi_disconnect());
-  ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA,&cfg));
-  ESP_ERROR_CHECK(esp_wifi_connect());
+  uint32_t gpio_num  = (uint32_t)arg;
+  xQueueSendFromISR(gpio_evt_queue,&gpio_num,NULL);
 }
 
-static esp_err_t event_handler(void * ctx, system_event_t * event)
+static void gpio_task_example(void *arg)
 {
-  switch(event->event_id)
-  {
-    case SYSTEM_EVENT_STA_START:
-       wifi_connect();
-       break;
-    case SYSTEM_EVENT_STA_GOT_IP:
-       xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
-       break;
-    case SYSTEM_EVENT_STA_DISCONNECTED:
-       esp_wifi_connect();
-       xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
-       break;
-    default:
-       break;
-  }
-  return ESP_OK;
-}
-
-static void initialise_wifi(void)
-{
-   esp_log_level_set("wifi", ESP_LOG_NONE); // disable wifi driver logging
-   tcpip_adapter_init();
-   wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-   ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
-   ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
-   ESP_ERROR_CHECK( esp_wifi_start() );
-}
-
-void tcp_client(void *pvParam)
-{
-  ESP_LOGI(TAG,"tcp_client_started\n");
-  struct sockaddr_in tcpServerAddr;
-  tcpServerAddr.sin_addr.s_addr = inet_addr(TCPServerIP);
-  tcpServerAddr.sin_family = AF_INET;
-  tcpServerAddr.sin_port = htons( 3010 );
-  int s,r;
-  char recv_buf[64];
-
-  while(1)
-  {
-   xEventGroupWaitBits(wifi_event_group,CONNECTED_BIT,false,true,portMAX_DELAY);
-   s = socket(AF_INET, SOCK_STREAM, 0);
-
-   if(s<0)
-   {
-    ESP_LOGE(TAG, "... Failed to allocate socket.\n");
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-    continue; 
-   }
-   ESP_LOGI(TAG,"....allocated socket \n");
-
-   if(connect(s, (struct sockaddr *)&tcpServerAddr, sizeof(tcpServerAddr)) != 0) {
-      ESP_LOGE(TAG, "... socket connect failed errno=%d \n", errno);
-      close(s);
-      vTaskDelay(4000 / portTICK_PERIOD_MS);
-      continue;
+    uint32_t io_num;
+    for(;;) {
+        if(xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+            printf("GPIO[%d] intr, val: %d\n", io_num, gpio_get_level(io_num));
+        }
     }
-   
-   ESP_LOGI(TAG, "... connected \n");
-    if( write(s , MESSAGE , strlen(MESSAGE)) < 0)
-      {
-        ESP_LOGE(TAG, "... Send failed \n");
-        close(s);
-        vTaskDelay(4000 / portTICK_PERIOD_MS);
-        continue;
-      }
-  ESP_LOGI(TAG, "... socket send sucess  \n");
-  do {
-            bzero(recv_buf, sizeof(recv_buf));
-            r = read(s, recv_buf, sizeof(recv_buf)-1);
-            for(int i = 0; i < r; i++) {
-                putchar(recv_buf[i]);
-            }
-        } while(r > 0);
-        ESP_LOGI(TAG, "... done reading from socket. Last read return=%d errno=%d\r\n", r, errno);
-        close(s);
-        ESP_LOGI(TAG, "... new request in 5 seconds");
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
-    }
-    ESP_LOGI(TAG, "...tcp_client task closed\n");
-  
-  }
-  
 }
 
-// Need to debbug this code 
+
+
 void app_main(void)
 {
-    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
-    wifi_event_group = xEventGroupCreate();
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES) {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ret = nvs_flash_init();
+    gpio_config_t io_conf;
+    //disable interrupt
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    //set as output mode
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    //bit mask of the pins that you want to set,e.g.GPIO18/19
+    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+    //disable pull-down mode
+    io_conf.pull_down_en = 0;
+    //disable pull-up mode
+    io_conf.pull_up_en = 0;
+    //configure GPIO with the given settings
+    gpio_config(&io_conf);
+
+    //interrupt of rising edge
+    io_conf.intr_type = GPIO_INTR_POSEDGE;
+    //bit mask of the pins, use GPIO4/5 here
+    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+    //set as input mode
+    io_conf.mode = GPIO_MODE_INPUT;
+    //enable pull-up mode
+    io_conf.pull_up_en = 1;
+    gpio_config(&io_conf);
+
+    //change gpio interrupt type for one pin
+    gpio_set_intr_type(GPIO_INPUT_IO_0, GPIO_INTR_ANYEDGE);
+
+    //create a queue to handle gpio event from isr
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+    //start gpio task
+    xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
+
+    //install gpio isr service
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
+    //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_INPUT_IO_1, gpio_isr_handler, (void*) GPIO_INPUT_IO_1);
+
+    //remove isr handler for gpio number.
+    gpio_isr_handler_remove(GPIO_INPUT_IO_0);
+    //hook isr handler for specific gpio pin again
+    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
+
+    printf("Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
+
+    int cnt = 0;
+    while(1) {
+        printf("cnt: %d\n", cnt++);
+        vTaskDelay(1000 / portTICK_RATE_MS);
+        gpio_set_level(GPIO_OUTPUT_IO_0, cnt % 2);
+        gpio_set_level(GPIO_OUTPUT_IO_1, cnt % 2);
     }
-    ESP_ERROR_CHECK( ret );
-    initialise_wifi();
-    xTaskCreate(&tcp_client,"tcp_client",4048,NULL,5,NULL);
+
+
+
 }
