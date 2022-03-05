18d25256f0dd9648e0e520176af3bbe5205928c9
DESKTOP-6R2O3C2\CXC <robo.12@outlook.es>
2022-03-05 15:20:00 -0600

    memory Esp32

--- main/main.c
+++ main/main.c
@@ -1,32 +1,26 @@
 #include <stdio.h>
 #include <esp_log.h>  
+#include "esp_heap_caps.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
 
+#define TAG "Memory"
 
-#define TAG "CONFIG"
+int x = 3;
+int y = 0;
+int data = 3;
+char bss[100];
+
 
 void app_main(void)
 {
-  ESP_LOGI(TAG,"MY_INT %d",CONFIG_MY_INT);
-  ESP_LOGI(TAG,"MY_STRING %s",CONFIG_MY_STRING);
-   
-  bool my_bool = false;
-  #ifdef CONFIG_MY_BOOL
-   my_bool = true;
-  #else
-   my_bool = false;
-  #endif
+  printf("%d %s",data,bss);
+  ESP_LOGI(TAG,"xPortGetHeapSize = %d DRAM",xPortGetFreeHeapSize());
 
-  ESP_LOGI(TAG,"MY_BOOL %s",my_bool ? "yes" : "no");
-  int option = 0;
+  int DRam = heap_caps_get_free_size(MALLOC_CAP_8BIT);
+  int IRam = heap_caps_get_free_size(MALLOC_CAP_32BIT) - heap_caps_get_free_size(MALLOC_CAP_8BIT);
 
-  #ifdef CONFIG_OPTION_1
-   option = 1;
-  #elif CONFIG_OPTION_2
-   option = 2;
-  #else 
-   option = 3;
-  #endif
-
- ESP_LOGI(TAG,"MY_OPTION %d",option);
+  ESP_LOGI(TAG,"DRAM \t \t %d"  ,DRam);
+  ESP_LOGI(TAG, "IRam \t \t %d" ,IRam);
 
 }
