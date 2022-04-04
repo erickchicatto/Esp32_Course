d991ffe1bdc18248a56dd3c9ba28edceb05f0e5b
DESKTOP-6R2O3C2\CXC <robo.12@outlook.es>
2022-04-04 18:26:03 -0500

    pussh button

--- main/main.c
+++ main/main.c
@@ -4,19 +4,27 @@
 #include "freertos/task.h"
 
 
-#define PIN 2
+#define PIN_LED     2
+#define PIN_SWITCH  15
 
 void app_main()
 {
-  gpio_pad_select_gpio(PIN);
-  gpio_set_direction(PIN,GPIO_MODE_OUTPUT);
+  gpio_pad_select_gpio(PIN_LED);
+  gpio_set_direction(PIN_LED,GPIO_MODE_OUTPUT);
+  
+  gpio_pad_select_gpio(PIN_SWITCH);
+  gpio_set_direction(PIN_SWITCH,GPIO_MODE_INPUT);
 
-  int isOn = 0;
+  gpio_pullup_en(PIN_SWITCH);
+  //gpio_pullup_dis(PIN_SWITCH);
+
+  
   while(true)
   {
-    isOn = !isOn;
-    gpio_set_level(PIN,isOn);
-    vTaskDelay(1000/portTICK_PERIOD_MS);
+    //isOn = !isOn; simulate push button
+    int level = gpio_get_level(PIN_SWITCH);
+    gpio_set_level(PIN_LED,level);
+    vTaskDelay(1);
   }
 
 
