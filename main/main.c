2de81ff963ce34188f104e1f2e725bb6387050e7
DESKTOP-6R2O3C2\CXC <robo.12@outlook.es>
2022-04-04 19:59:10 -0500

    PWM

--- main/main.c
+++ main/main.c
@@ -1,28 +1,47 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/gpio.h"
-#include "driver/touch_pad.h"
+#include "driver/ledc.h"
+#include "esp_err.h"
+
 
 
 
 void app_main()
 {
-  touch_pad_init();
-  touch_pad_set_voltage(TOUCH_HVOLT_2V7, TOUCH_LVOLT_0V5, TOUCH_HVOLT_ATTEN_1V);
-  touch_pad_config(TOUCH_PAD_SLOPE_MAX, -1);
 
-  uint16_t val;
-  uint16_t touch_filter_value = 0;
-  uint16_t touch_rawValue = 0;
+
+//Init the driver 
+  ledc_timer_config_t timer = {
+      .speed_mode = LEDC_LOW_SPEED_MODE,
+      .duty_resolution = LEDC_TIMER_10_BIT,
+      .timer_num = LEDC_TIMER_0,
+      .freq_hz = 5000,
+      .clk_cfg = LEDC_AUTO_CLK};
+
+  ledc_timer_config(&timer);
+
+   ledc_channel_config_t channel = {
+      .gpio_num = 4,
+      .speed_mode = LEDC_LOW_SPEED_MODE,
+      .channel = LEDC_CHANNEL_0,
+      .timer_sel = LEDC_TIMER_0,
+      .duty = 0,
+      .hpoint = 0};
+  ledc_channel_config(&channel);
+
+  ledc_fade_func_install(0);
+   
+  for(int i=0;i<1024;i++)
+  {
+    ledc_set_duty_and_update(LEDC_LOW_SPEED_MODE,LEDC_CHANNEL_0,i,0);
+    vTaskDelay(10/portTICK_PERIOD_MS);
+  }
 
   while(true)
   {
-    touch_pad_read_raw_data(TOUCH_PAD_SLOPE_MAX, &touch_rawValue);
-    touch_pad_read_filtered(TOUCH_PAD_SLOPE_MAX, &touch_filter_value);
-    touch_pad_read(TOUCH_PAD_SLOPE_MAX, &val);
-    printf("val = %d raw = %d filtered = %d\n", val, touch_rawValue, touch_filter_value);
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    ledc_set_fade_time_and_start(LEDC_LOW_SPEED_MODE,LEDC_CHANNEL_0,0,1000,LEDC_FADE_WAIT_DONE);
+    ledc_set_fade_time_and_start(LEDC_LOW_SPEED_MODE,LEDC_CHANNEL_0,1024,1000,LEDC_FADE_WAIT_DONE);
   }
 
 
